910e0588f5385401d0cdad01b00c416fec65e3bc
AakashShahi <shahiaakash112@gmail.com>
2025-05-15 21:31:08 +0545

    Google and fb login sign added

--- dev/kaammaa/lib/view/login_view.dart
+++ dev/kaammaa/lib/view/login_view.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:kaammaa/view/signup_view.dart';
 
@@ -14,88 +15,159 @@ class _LoginviewState extends State<Loginview> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            padding: EdgeInsets.all(15),
-            color: Colors.white,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Image.asset(
-                    "assets/logo/backlogo.png",
-                    height: 40,
-                    width: 40,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(15),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () async {
+                          final shouldExit = await showDialog<bool>(
+                            context: context,
+                            builder:
+                                (context) => AlertDialog(
+                                  title: const Text('Exit App'),
+                                  content: const Text(
+                                    'Do you want to exit the app?',
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed:
+                                          () =>
+                                              Navigator.of(context).pop(false),
+                                      child: const Text(
+                                        'Cancel',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed:
+                                          () => Navigator.of(context).pop(true),
+                                      child: const Text(
+                                        'Exit',
+                                        style: TextStyle(color: Colors.red),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                          );
+
+                          if (shouldExit == true) {
+                            exit(0);
+                          }
+                        },
+                        icon: Image.asset(
+                          "assets/logo/backlogo.png",
+                          height: 40,
+                          width: 40,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Login to your account",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            _buildTextField(
+                              labelText: "Enter your email",
+                              obscureText: false,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please enter your email";
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 15),
+                            _buildTextField(
+                              labelText: "Enter your password",
+                              obscureText: _obscurePassword,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please enter your password";
+                                }
+                                return null;
+                              },
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscurePassword
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                  color: Colors.black,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _obscurePassword = !_obscurePassword;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            fontFamily: "Inter",
+                            color: const Color(0xFFFA5804),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      _buildSignInButton(size),
+                      const SizedBox(height: 40),
+                      Center(
+                        child: Image.asset(
+                          "assets/images/continuewith.png",
+                          width: size.width * 0.8,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/logo/google.png",
+                            width: size.width * 0.1,
+                          ),
+                          SizedBox(width: size.width * 0.1),
+                          Image.asset(
+                            "assets/logo/fb.png",
+                            width: size.width * 0.1,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      _buildSignUpOption(),
+                      const Spacer(),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 20),
-                const Text(
-                  "Login to your account",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontFamily: "Inter",
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                _buildTextField(
-                  labelText: "Enter your email",
-                  obscureText: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your email";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 15),
-                _buildTextField(
-                  labelText: "Enter your password",
-                  obscureText: _obscurePassword,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your password";
-                    }
-                    return null;
-                  },
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscurePassword
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                      color: Colors.black,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _obscurePassword = !_obscurePassword;
-                      });
-                    },
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      fontFamily: "Inter",
-                      color: Color(0xFFFA5804),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                _buildSignInButton(),
-                const SizedBox(height: 20),
-                _buildSignUpOption(),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -128,10 +200,10 @@ class _LoginviewState extends State<Loginview> {
     );
   }
 
-  Widget _buildSignInButton() {
+  Widget _buildSignInButton(Size size) {
     return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.8,
+      child: FractionallySizedBox(
+        widthFactor: 0.8,
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
             backgroundColor: const Color(0xFFFA5804),
--- dev/kaammaa/lib/view/signup_view.dart
+++ dev/kaammaa/lib/view/signup_view.dart
@@ -15,109 +15,135 @@ class _SignupviewState extends State<Signupview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(15),
-          color: Colors.white,
-          height: MediaQuery.of(context).size.height,
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context); // Navigate back
-                    },
-                    icon: Image.asset(
-                      "assets/logo/backlogo.png",
-                      height: 40,
-                      width: 40,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Register to new account",
-                    style: TextStyle(fontSize: 24, fontFamily: "Inter"),
-                  ),
-                  const SizedBox(height: 20),
-                  _buildTextField(
-                    labelText: "Enter your username",
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your username";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  _buildTextField(
-                    labelText: "Enter your email",
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your email";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 15),
-                  _buildTextField(
-                    labelText: "Enter your password",
-                    obscureText: _obscurePassword,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter your password";
-                      }
-                      return null;
-                    },
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                        color: Colors.black,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  Center(
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFFA5804),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+        child: LayoutBuilder(
+          builder:
+              (context, constraints) => SingleChildScrollView(
+                padding: const EdgeInsets.all(15),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            onPressed: () => Navigator.pop(context),
+                            icon: Image.asset(
+                              "assets/logo/backlogo.png",
+                              height: 40,
+                              width: 40,
+                            ),
                           ),
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                        ),
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            // Handle signup logic here
-                          }
-                        },
-                        child: const Text(
-                          "Sign-Up",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "Inter",
-                            fontSize: 18,
+                          const SizedBox(height: 20),
+                          const Text(
+                            "Register to new account",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Inter",
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
+                          const SizedBox(height: 20),
+                          _buildTextField(
+                            labelText: "Enter your username",
+                            validator:
+                                (value) =>
+                                    value == null || value.isEmpty
+                                        ? "Please enter your username"
+                                        : null,
+                          ),
+                          const SizedBox(height: 20),
+                          _buildTextField(
+                            labelText: "Enter your email",
+                            validator:
+                                (value) =>
+                                    value == null || value.isEmpty
+                                        ? "Please enter your email"
+                                        : null,
+                          ),
+                          const SizedBox(height: 15),
+                          _buildTextField(
+                            labelText: "Enter your password",
+                            obscureText: _obscurePassword,
+                            validator:
+                                (value) =>
+                                    value == null || value.isEmpty
+                                        ? "Please enter your password"
+                                        : null,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscurePassword
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                              onPressed:
+                                  () => setState(
+                                    () => _obscurePassword = !_obscurePassword,
+                                  ),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Center(
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFFA5804),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    // Handle signup
+                                  }
+                                },
+                                child: const Text(
+                                  "Sign-Up",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                          Center(
+                            child: Image.asset(
+                              "assets/images/continuewith.png",
+                              width: MediaQuery.of(context).size.width * 0.8,
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                "assets/logo/google.png",
+                                width: MediaQuery.of(context).size.width * 0.1,
+                              ),
+                              const SizedBox(width: 30),
+                              Image.asset(
+                                "assets/logo/fb.png",
+                                width: MediaQuery.of(context).size.width * 0.1,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          _buildSignUpOption(context),
+                        ],
                       ),
                     ),
                   ),
-                  _buildSignUpOption(context),
-                ],
+                ),
               ),
-            ),
-          ),
         ),
       ),
     );
@@ -133,53 +159,47 @@ class _SignupviewState extends State<Signupview> {
       obscureText: obscureText,
       decoration: InputDecoration(
         labelText: labelText,
-        labelStyle: const TextStyle(color: Colors.black),
         filled: true,
         fillColor: const Color(0xFFD9D9D9),
-        enabledBorder: OutlineInputBorder(
+        border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
           borderSide: BorderSide.none,
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
-          borderSide: const BorderSide(color: Color(0xFFFA5804), width: 1.5),
+          borderSide: BorderSide(color: Color(0xFFFA5804), width: 1.5),
         ),
         suffixIcon: suffixIcon,
       ),
       validator: validator,
     );
   }
-}
 
-Widget _buildSignUpOption(BuildContext context) {
-  return Center(
-    child: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const Text(
-          "Already in ",
-          style: TextStyle(fontFamily: "Inter", fontSize: 16),
-        ),
-        Image.asset("assets/logo/kaammaa_txt.png", height: 100, width: 100),
-        const SizedBox(width: 5),
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Loginview()),
-            );
-          },
-          child: const Text(
-            "Sign in",
-            style: TextStyle(
-              color: Color(0xFFFA5804),
-              fontFamily: "Inter",
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
+  Widget _buildSignUpOption(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("Already in ", style: TextStyle(fontSize: 16)),
+          Image.asset("assets/logo/kaammaa_txt.png", height: 100, width: 100),
+          const SizedBox(width: 5),
+          GestureDetector(
+            onTap:
+                () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const Loginview()),
+                ),
+            child: const Text(
+              "Sign in",
+              style: TextStyle(
+                color: Color(0xFFFA5804),
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
