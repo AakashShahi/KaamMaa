f3f88b1f60c3be77373ff72fa68f0ae97358635b
AakashShahi <shahiaakash112@gmail.com>
2025-05-18 19:42:19 +0545

    Prolem on tab view fixed

--- dev/kaammaa/lib/view/onboarding_view.dart
+++ dev/kaammaa/lib/view/onboarding_view.dart
@@ -25,126 +25,129 @@ class _OnboardingViewState extends State<OnboardingView> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView(
-                controller: controller,
-                onPageChanged: (index) {
-                  setState(() => isLastPage = index == 2);
-                },
-                children: [
-                  Image.asset(
-                    "assets/images/onboard1.png",
-                    fit: BoxFit.cover,
-                    width: screenWidth,
-                    height: screenHeight,
-                  ),
-                  Image.asset(
-                    "assets/images/onboard2.png",
-                    fit: BoxFit.cover,
-                    width: screenWidth,
-                    height: screenHeight,
-                  ),
-                  Image.asset(
-                    "assets/images/onboard3.png",
-                    fit: BoxFit.cover,
-                    width: screenWidth,
-                    height: screenHeight,
-                  ),
-                ],
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: controller,
+              onPageChanged: (index) {
+                setState(() => isLastPage = index == 2);
+              },
+              children: [
+                _buildPage("assets/images/onboard1.png"),
+                _buildPage("assets/images/onboard2.png"),
+                _buildPage("assets/images/onboard3.png"),
+              ],
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              color: Colors.white,
-              child:
-                  isLastPage
-                      ? SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: screenWidth * 0.05,
+              vertical: screenHeight * 0.015,
+            ),
+            color: Colors.white,
+            child:
+                isLastPage
+                    ? SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFFA5804),
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.02,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.045,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => const SelectionView(),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                    : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            controller.jumpToPage(2);
+                          },
+                          child: Text(
+                            "Skip",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: screenWidth * 0.04,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        SmoothPageIndicator(
+                          controller: controller,
+                          count: 3,
+                          effect: JumpingDotEffect(
+                            dotHeight: screenWidth * 0.03,
+                            dotWidth: screenWidth * 0.03,
+                            spacing: screenWidth * 0.03,
+                            jumpScale: 1.5,
+                            verticalOffset: screenWidth * 0.03,
+                            dotColor: const Color(0xFFD3D3D3),
+                            activeDotColor: const Color(0xFFFA5804),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            controller.nextPage(
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeInOut,
+                            );
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: const Color(0xFFFA5804),
-                            padding: const EdgeInsets.symmetric(vertical: 15),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: screenHeight * 0.015,
+                              horizontal: screenWidth * 0.05,
+                            ),
                           ),
-                          child: const Text(
-                            "Get Started",
-                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          child: Text(
+                            "Next",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: screenWidth * 0.04,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                builder: (context) => const SelectionView(),
-                              ),
-                            );
-                          },
                         ),
-                      )
-                      : Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              controller.jumpToPage(2);
-                            },
-                            child: const Text(
-                              "Skip",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                          SmoothPageIndicator(
-                            controller: controller,
-                            count: 3,
-                            effect: JumpingDotEffect(
-                              dotHeight: 12,
-                              dotWidth: 12,
-                              spacing: 12,
-                              jumpScale: 1.5,
-                              verticalOffset: 12,
-                              dotColor: const Color(0xFFD3D3D3),
-                              activeDotColor: const Color(0xFFFA5804),
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              controller.nextPage(
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFFFA5804),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 10,
-                                horizontal: 20,
-                              ),
-                            ),
-                            child: const Text(
-                              "Next",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-            ),
-          ],
-        ),
+                      ],
+                    ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildPage(String imagePath) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Image.asset(
+      imagePath,
+      width: screenWidth,
+      height: screenHeight,
+      fit: BoxFit.cover,
     );
   }
 }
